8f32ef821c040243349f20fd56a408be03bfab3d
jai <jaikumar.d@origamicreative.com>
2023-01-10 18:33:36 +0530

    update url from server

--- client/script.js
+++ client/script.js
@@ -86,7 +86,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv)
 
-    const response = await fetch('https://origamichatgpt.onrender.comclea', {
+    const response = await fetch('https://origamichatgpt.onrender.com', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
